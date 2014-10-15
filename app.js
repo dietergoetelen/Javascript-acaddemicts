ac1d7a516f838403d52ff756bf93ff06244e79a6
Dieter Goetelen <dieter.goetelen@hotmail.com>
2014-10-15 12:28:52 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+
+var app = require('./config/express')();
+
+// Routes
+require('./server/router')(app);
+
+// Errorhandlers
+require('./config/errorHandler')(app);
+
+app.listen(5000, function () { console.log('server listening on port', 5000); });
